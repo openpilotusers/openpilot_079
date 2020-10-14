2d6dbdce2484c8df4af78f2e18c196039955fbc4
Vehicle Researcher <>
2020-10-15 02:57:14 +0900

    openpilot v0.7.9-release

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -204,16 +204,16 @@ bool addr_safety_check(CAN_FIFOMailBox_TypeDef *to_push,
 
 void generic_rx_checks(bool stock_ecu_detected) {
   // exit controls on rising edge of gas press
-  if (gas_pressed && !gas_pressed_prev && !(unsafe_mode & UNSAFE_DISABLE_DISENGAGE_ON_GAS)) {
-    controls_allowed = 0;
-  }
-  gas_pressed_prev = gas_pressed;
+  //if (gas_pressed && !gas_pressed_prev && !(unsafe_mode & UNSAFE_DISABLE_DISENGAGE_ON_GAS)) {
+  //  controls_allowed = 0;
+  //}
+  //gas_pressed_prev = gas_pressed;
 
   // exit controls on rising edge of brake press
-  if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
-    controls_allowed = 0;
-  }
-  brake_pressed_prev = brake_pressed;
+  //if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
+  //  controls_allowed = 0;
+  //}
+  //brake_pressed_prev = brake_pressed;
 
   // check if stock ECU is on bus broken by car harness
   if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && stock_ecu_detected) {
