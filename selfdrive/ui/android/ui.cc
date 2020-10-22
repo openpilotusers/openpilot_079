0c067fbb6aa88324d6feeb28b73ae90da9ecb416
Vehicle Researcher <>
2020-10-23 08:57:34 +0900

    openpilot v0.7.9-release

--- selfdrive/ui/android/ui.cc
+++ selfdrive/ui/android/ui.cc
@@ -178,9 +178,7 @@ int main(int argc, char* argv[]) {
     if (s->started || s->ignition) {
       if (s->scene.params.nOpkrAutoScreenOff) {
         // turn on screen when alert is here.
-        auto alert_sound = s->scene.controls_state.getAlertSound();
-        auto alertStatus = s->scene.controls_state.getAlertStatus();
-        if (s->awake_timeout == 0 && (s->status == STATUS_DISENGAGED || s->status == STATUS_ALERT || s->status == STATUS_WARNING || alert_sound != AudibleAlert::NONE || alertStatus == cereal::ControlsState::AlertStatus::NORMAL)) {
+        if (s->awake_timeout == 0 && (s->status == STATUS_DISENGAGED || s->status == STATUS_ALERT || s->status == STATUS_WARNING || (s->scene.alert_text1 != ""))) {
           set_awake(s, true);
         }
       } else {
